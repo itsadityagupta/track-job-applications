3afeaa20270d78b38140dfac34969174401eb904
Aditya-Gupta1 <aditygupta@deloitte.com>
2022-11-07 19:23:33 +0530

    fix: adding some more cases for Status Enum

--- track/app_constants.py
+++ track/app_constants.py
@@ -19,18 +19,25 @@ class Status(Enum):
 
         if status.upper() == "APPLIED":
             return Status.APPLIED
-        elif status.upper() in ["ONLINE_ASSESSMENT", "OA"]:
+        elif status.upper() in [
+            "ONLINE_ASSESSMENT",
+            "OA",
+            "ONLINE-ASSESSMENT",
+        ]:
             return Status.OA
         elif status.upper() in [
             "TECH_INTERVIEW",
             "TECH INTERVIEW",
+            "TECH-INTERVIEW",
             "TECH ROUND",
             "TECH_ROUND",
+            "TECH-ROUND",
+            "TECH",
         ]:
             return Status.TECH_INTERVIEW
-        elif status.upper() in ["HR_ROUND", "HR ROUND"]:
+        elif status.upper() in ["HR_ROUND", "HR ROUND", "HR-ROUND"]:
             return Status.HR_ROUND
-        elif status.upper() == "REJECTED":
+        elif status.upper() in ["REJECTED"]:
             return Status.REJECTED
         else:
             raise ValueError(f"'{status}' is not a Valid Status")
